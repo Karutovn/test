6fc59d12e08f033c2ce758ed5bab3acf2dfdb633
Karuto <nguyenthaison609@gmail.com>
2024-04-12 15:42:16 +0700

    add

---
+++ src/en/rudrascans/src/eu/kanade/tachiyomi/extension/en/rudrascans/RudraScans.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.en.rudrascans
+
+import eu.kanade.tachiyomi.multisrc.keyoapp.Keyoapp
+
+class RudraScans : Keyoapp("Rudra Scans", "https://rudrascans.com", "en")
---
+++ src/vi/tusachxinhxinh/src/eu/kanade/tachiyomi/extension/vi/tusachxinhxinh/tusachxinhxinh.kt
@@ -0,0 +1,5 @@
+package eu.kanade.tachiyomi.extension.vi.tusachxinhxinh
+
+import eu.kanade.tachiyomi.multisrc.a3manga
+
+class A3Manga : A3Manga("Tu sach xinh xinh", "https://tusachxinh.online", "vi")
