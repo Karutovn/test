9146943bf4b39145d9fa8b9d7f9aa9f0e93d426d
Karuto <nguyenthaison609@gmail.com>
2024-04-28 08:58:23 +0700

    Update tusachxinhxinh.kt

--- src/vi/tusachxinhxinh/src/eu/kanade/tachiyomi/extension/vi/tusachxinhxinh/tusachxinhxinh.kt
+++ src/vi/tusachxinhxinh/src/eu/kanade/tachiyomi/extension/vi/tusachxinhxinh/tusachxinhxinh.kt
@@ -2,4 +2,4 @@ package eu.kanade.tachiyomi.extension.vi.tusachxinhxinh
 
 import eu.kanade.tachiyomi.multisrc.a3manga
 
-class A3Manga : A3Manga("Tu sach xinh xinh", "https://tusachxinh.online", "vi")
+class Tusachxinhxinh : A3Manga("Tusachxinhxinh", "https://tusachxinh.online", "vi")
